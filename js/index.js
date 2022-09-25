7248ced81421b05c7c80b2ef7f58ae6ac37c42a0
WilliamTRich <112077180+WilliamTRich@users.noreply.github.com>
2022-09-24 19:11:02 -0700

    Added conversion function

--- js/index.js
+++ js/index.js
@@ -5,11 +5,16 @@ function changeLocation() {
 let cookieAccept = (element) => element.parentElement.remove();
 
 function tempSelect (element) {
-    let degrees = document.querySelectorAll(".red, .blue")
-    if (element.value === "fahrenheit") {
-        console.log(typeof degrees);
-        const converted = degrees.map((item) => {
-            return (parseInt(item.textContent) * 1.8 + 32);
-        })
+    let degrees = Object.values(document.querySelectorAll(".red, .blue"));
+    if(element.value === "fahrenheit") {
+        for (let i = 0; i < degrees.length; i++) {
+            let fahrenheit = Math.floor(parseInt(degrees[i].textContent) * 1.8 + 32);
+            degrees[i].textContent = fahrenheit;
+        }
+    } else if (element.value === "celsuis") {
+        for (let i = 0; i < degrees.length; i++) {
+        let celsuis = Math.floor((parseInt(degrees[i].textContent) - 32) / 1.8);
+        degrees[i].textContent = celsuis;
+        }
     }
 }
